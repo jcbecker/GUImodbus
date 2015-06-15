6090f6a23594f4d16896a5b086ce4bdc2724c71e
Marcelo Acordi <marcelopancotte@gmail.com>
2015-06-15 14:36:58 -0300

    Método checksum incorreto.

--- IO/ModBusReader.py
+++ IO/ModBusReader.py
@@ -1,15 +1,29 @@
 import time
 from serial import Serial
+from ModBusIO import ModBusIO
 
-class ModBusReader:
-			
-	def read(self):
+class ModBusReader(ModBusIO):
+
+	def __init__(self):
+		self.function = "03"
+
+	def _checkSum(self, reg, regNumber):
+		cs = int( self.slaveAddress, 16 ) + int( self.function, 16 ) 
+		cs = cs + int( reg, 16 ) + int( regNumber, 16 )
+		return hex(cs)[2:]
+
+	def read(self, reg, regNumber ):
 		
 		confFile = open("GUImodbus/Conf/conf.txt","r")
 		confFile.readline();
 		rport = confFile.readline().split('[')[1].split(']')[0]
 		
 		sPort = Serial(port = rport, baudrate = 9600, timeout = 1)
+		
+		msg = self.iniMSG + self.slaveAddress + self.function
+		msg = msg + reg + regNumber + self._checkSum(reg,regNumber) + self.endMSG
+		
+		print msg
 		
 		word = ""
 		while True:
--- IO/ModBusWriter.py
+++ IO/ModBusWriter.py
@@ -1,15 +1,26 @@
 import time
 from serial import Serial
+from ModBusIO import ModBusIO
 
-class ModBusWriter:
-		
+class ModBusWriter(ModBusIO):
+	
+	def __init__(self):
+		self.function = "06"
+
 	def write(self, t):
 		confFile = open("GUImodbus/Conf/conf.txt","r")
 		wport = confFile.readline().split('[')[1].split(']')[0]
 
-		teste = Serial(port = wport, baudrate = 9600)
-			
+		msg = self.iniMSG + self.slaveAddress + self.function
+
+		sComm = Serial(port = wport, baudrate = 9600)		
+		
 		try:
-			teste.write(t + b'\n'.encode())				
+			sComm.write( t)				
 		except Exception as e:
 			raise e
+			
+	def _checkSum(self, reg, regNumber):
+		cs = int( self.slaveAddress, 16 ) + int( self.function, 16 ) 
+		cs = cs + int( reg, 16 ) + int( regNumber, 16 )
+		return hex(cs)[2:]
