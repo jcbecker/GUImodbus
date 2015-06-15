7e44840d514c0d7dac5fb365734307dfe0caf7a2
Marcelo Acordi <marcelopancotte@gmail.com>
2015-06-15 15:01:58 -0300

    Manda requisição

--- IO/ModBusReader.py
+++ IO/ModBusReader.py
@@ -1,11 +1,13 @@
 import time
 from serial import Serial
 from ModBusIO import ModBusIO
+from ModBusWriter import ModBusWriter
 
 class ModBusReader(ModBusIO):
 
 	def __init__(self):
 		self.function = "03"
+		self.writer = ModBusWriter()
 
 	def _checkSum(self, reg, regNumber):
 		cs = int( self.slaveAddress, 16 ) + int( self.function, 16 ) 
@@ -18,12 +20,14 @@ class ModBusReader(ModBusIO):
 		confFile.readline();
 		rport = confFile.readline().split('[')[1].split(']')[0]
 		
-		sPort = Serial(port = rport, baudrate = 9600, timeout = 1)
+		sPort = Serial(port = rport, baudrate = 9600, timeout = 3)
 		
-		msg = self.iniMSG + self.slaveAddress + self.function
-		msg = msg + reg + regNumber + self._checkSum(reg,regNumber) + self.endMSG
+		query = self.iniMSG + self.slaveAddress + self.function
+		query = query + reg + regNumber + self._checkSum(reg,regNumber) + self.endMSG
 		
-		print msg
+		print query
+		
+		self.writer.write(query)
 		
 		word = ""
 		while True:
