baa8ac000af7f8943bd8e44fd1a156fe7a5e5763
Matheus DallRosa <matheusdallrosa94@gmail.com>
2015-06-17 17:30:41 -0300

    Teste para leitura e escrita.

--- testing.py
+++ testing.py
@@ -2,13 +2,17 @@ from Resources.Water import Water
 from Resources.Alarm import Alarm
 from IO.ModBusWriter import ModBusWriter
 from IO.ModBusReader import ModBusReader
+import time
 
 alarm = Alarm()
 
 alarm.fired()
 #monitReg = "000A"
 
+time.sleep(2)
+
 mw = ModBusWriter()
-mw.write("0009",0, "0001" )
+mw.write("0003", "0001" )
+
 alarm.fired()
 #mbw
