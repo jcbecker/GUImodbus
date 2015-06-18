252a5371492f5a64a7bf03bae3475ec60839089b
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-06-18 20:09:23 -0300

    Teste do método checkAlarm.

--- testing.py
+++ testing.py
@@ -4,7 +4,29 @@ from IO.ModBusWriter import ModBusWriter
 from IO.ModBusReader import ModBusReader
 import time
 
-#teste da ligacao do alarme e da checagem
+#--inicio do teste da verificacao do alarme em todos os locais.
+a = Alarm()
+if a.checkAlarm("garagem"):
+	print "O alarme da garagem esta ligado."
+else:
+	print "O alarme da garagem esta desligado."
+
+mw = ModBusWriter()
+if mw.write("000A","0002"):
+	print "O alarme da piscina foi aceso."
+
+if a.checkAlarm("piscina1"):
+	print "O alarme da piscina esta ligado."
+else:
+	print "O alarme da piscina esta desligado."
+
+if a.checkAlarm("cozinha"):
+	print "O alarme da cozinha esta ligado."
+else:
+	print "O alarme da cozinha esta desligado."
+#--fim do teste da verificacao do alarme em todos os locais.
+
+#--inicio do teste da ligacao do alarme e da checagem
 #para verificar se o alarme esta ligado ou nao
 #a = Alarm()
 
@@ -15,8 +37,9 @@ import time
 
 #	if a.ONOFF():
 #		print "Agora o alarme foi ligado."
+#--fim do teste da ligacao do alarme e da checagem
 
-#teste do disparo do alarme
+#--inicio do teste do disparo do alarme
 #mw = ModBusWriter()
 #if mw.write("0009", "0000" ):
 	#print "Os dados foram escritos." 
@@ -38,5 +61,5 @@ import time
 #	print "O alarme esta disparado"
 #else:
 #	print "O alarme nao esta disparado"
-
+#--fim do teste do disparo do alarme
 #mbw
