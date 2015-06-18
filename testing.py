f62be445ef778772f1830681ee6541bb282723a9
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-06-18 18:04:11 -0300

    Teste dos métodos checkONOFF e ONOFF.

--- testing.py
+++ testing.py
@@ -4,26 +4,39 @@ from IO.ModBusWriter import ModBusWriter
 from IO.ModBusReader import ModBusReader
 import time
 
+#teste da ligacao do alarme e da checagem
+#para verificar se o alarme esta ligado ou nao
 #a = Alarm()
 
-#a.ONOFF()
+#if a.checkONOFF():
+#	print "O alarme agora esta ligado."
+#else:
+#	print "O alarme agora esta desligado."
+
+#	if a.ONOFF():
+#		print "Agora o alarme foi ligado."
 
 #teste do disparo do alarme
-mw = ModBusWriter()
-print "Resposta da escrita: " +mw.write("0009", "0001" )
+#mw = ModBusWriter()
+#if mw.write("0009", "0000" ):
+	#print "Os dados foram escritos." 
+#else:
+	#print "Os dados nao foram escritos."
 
-a = Alarm()
-if a.fired():
-	print "O alarme esta disparado"
-else:
-	print "O alarme nao esta disparado"
+#a = Alarm()
+#if a.fired():
+#	print "O alarme esta disparado"
+#else:
+#	print "O alarme nao esta disparado"
 
-print "Resposta da escrita: " +mw.write("0009", "0002" )
+#if mw.write("0009", "0002" ):
+#	print "Os dados foram escritos." 
+#else:
+#	print "Os dados nao foram escritos."
 
-a = Alarm()
-if a.fired():
-	print "O alarme esta disparado"
-else:
-	print "O alarme nao esta disparado"
+#if a.fired():
+#	print "O alarme esta disparado"
+#else:
+#	print "O alarme nao esta disparado"
 
 #mbw
