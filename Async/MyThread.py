a9dce8901aac997843bfa3599eff4b0d9665ba0b
marcoputon <marcoputon@gmail.com>
2015-06-19 20:19:43 -0300

    Esperar para ler devolta.

--- Async/MyThread.py
+++ Async/MyThread.py
@@ -7,9 +7,8 @@ class MyThread(Thread):
 		Thread.__init__(self)
 		self.monitors = m
 
-	#como as thread iram fazer somente leitura
-	#nao precisa controlar o sincronismo.
 	def run(self):
 		while True:
 			for i in self.monitors:
 				i.myTurn()
+			time.sleep(5)
