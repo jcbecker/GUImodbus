74c6149a4d1ff7d9683919bac0513a3777152f79
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-06-19 00:06:19 -0300

    Teste da classe AlarmMonitor.

--- testingThread.py
+++ testingThread.py
@@ -1,10 +1,14 @@
 from Async.MyThread import MyThread
 from Async.Monitor import Monitor
+from Async.AlarmMonitor import AlarmMonitor
 from Resources.Temperature import Temperature
 from Resources.Lamp import Lamp
+from Resources.Alarm import Alarm
 
-t = MyThread("Temperature", Monitor( "Temperature", Temperature() ) )
-t2 = MyThread("Lamp", Monitor( "Lamp", Lamp() ) )
+t = MyThread( Monitor( "Temperature", Temperature() ) )
+
+alarm = AlarmMonitor( "Alarm", Alarm() )
+t2 = MyThread( alarm )
 
 t.start()
 t2.start()
