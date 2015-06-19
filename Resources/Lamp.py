6e292a402232d3dfedd0f8b83023ebae4b068ba3
matheus dallrosa <matheusdallrosa94@gmail.com>
2015-06-18 23:40:24 -0300

    Atributos novos.

--- Resources/Lamp.py
+++ Resources/Lamp.py
@@ -5,106 +5,105 @@ class Lamp:
 
 	monitReg11 = {}
 	monitReg12 = {}
-	monitReg13 = {}	
-
+	monitReg13 = {}
 	comReg14 = {}
 	comReg15 = {}
 	comReg16 = {}
 
 	def __init__(self):
-		monitReg11['saladeestar1'] = 0
-		monitReg11['saladeestar2'] = 1
-		monitReg11['saladeestar3'] = 2
-		monitReg11['saladejantar1'] = 3
-		monitReg11['saladejantar2'] = 4
-		monitReg11['saladejogos1'] = 5
-		monitReg11['saladejogos2'] = 6
-		monitReg11['dormitorio1'] = 7
+		self.monitReg11['saladeestar1'] = 0
+		self.monitReg11['saladeestar2'] = 1
+		self.monitReg11['saladeestar3'] = 2
+		self.monitReg11['saladejantar1'] = 3
+		self.monitReg11['saladejantar2'] = 4
+		self.monitReg11['saladejogos1'] = 5
+		self.monitReg11['saladejogos2'] = 6
+		self.monitReg11['dormitorio1'] = 7
 
-		monitReg12['bwc1'] = 0
-		monitReg12['circulacao'] = 1
-		monitReg12['dormitorio2'] = 2
-		monitReg12['closed'] = 3
-		monitReg12['bwc2'] = 4
-		monitReg12['suite1'] = 5
-		monitReg12['suite2'] = 6
-		monitReg12['suite3'] = 7
+		self.monitReg12['bwc1'] = 0
+		self.monitReg12['circulacao'] = 1
+		self.monitReg12['dormitorio2'] = 2
+		self.monitReg12['closed'] = 3
+		self.monitReg12['bwc2'] = 4
+		self.monitReg12['suite1'] = 5
+		self.monitReg12['suite2'] = 6
+		self.monitReg12['suite3'] = 7
 		
-		monitReg13['bwc'] = 0
-		monitReg13['deposito'] = 1
-		monitReg13['varandadapiscina'] = 2
-		monitReg13['piscina1'] = 3
-		monitReg13['piscina2'] = 4
-		monitReg13['piscina3'] = 5
+		self.monitReg13['bwc'] = 0
+		self.monitReg13['deposito'] = 1
+		self.monitReg13['varandadapiscina'] = 2
+		self.monitReg13['piscina1'] = 3
+		self.monitReg13['piscina2'] = 4
+		self.monitReg13['piscina3'] = 5
 
-		comReg14['saladeestar1'] = 0
-		comReg14['saladeestar1'] = 1
-		comReg14['saladeestar1'] = 2
-		comReg14['saladeestar1'] = 3
-		comReg14['saladeestar1'] = 4
-		comReg14['saladeestar1'] = 5
-		comReg14['saladeestar1'] = 6
-		comReg14['saladeestar1'] = 7
+		self.comReg14['saladeestar1'] = 0
+		self.comReg14['saladeestar1'] = 1
+		self.comReg14['saladeestar1'] = 2
+		self.comReg14['saladeestar1'] = 3
+		self.comReg14['saladeestar1'] = 4
+		self.comReg14['saladeestar1'] = 5
+		self.comReg14['saladeestar1'] = 6
+		self.comReg14['saladeestar1'] = 7
 		
-		comReg15['bwc1'] = 0
-		comReg15['circulacao'] = 1
-		comReg15['dormitorio2'] = 2
-		comReg15['closed'] = 3
-		comReg15['bwc2'] = 4
-		comReg15['suite1'] = 5
-		comReg15['suite2'] = 6
-		comReg15['suite3'] = 7
+		self.comReg15['bwc1'] = 0
+		self.comReg15['circulacao'] = 1
+		self.comReg15['dormitorio2'] = 2
+		self.comReg15['closed'] = 3
+		self.comReg15['bwc2'] = 4
+		self.comReg15['suite1'] = 5
+		self.comReg15['suite2'] = 6
+		self.comReg15['suite3'] = 7
 
-		comReg16['bwc'] = 0
-		comReg16['deposito'] = 1
-		comReg16['varandadapiscina'] = 2
-		comReg16['piscina1'] = 3
-		comReg16['piscina2'] = 4
-		comReg16['piscina3'] = 5
+		self.comReg16['bwc'] = 0
+		self.comReg16['deposito'] = 1
+		self.comReg16['varandadapiscina'] = 2
+		self.comReg16['piscina1'] = 3
+		self.comReg16['piscina2'] = 4
+		self.comReg16['piscina3'] = 5
 
 	def monitLamps1(self,place):
-		if monitReg11.has_key(place):
-			reg = monitReg11[place]
+		if self.monitReg11.has_key(place):
+			reg = self.monitReg11[place]
 
 			#pedir informacao
 		else:
 			return None
 
 	def monitLamps2(self,place):
-		if monitReg12.has_key(place):
-			reg = monitReg12[place]
+		if self.monitReg12.has_key(place):
+			reg = self.monitReg12[place]
 
 			#pedir informacao
 		else:
 			return None
 
 	def monitLamps3(self,place):
-		if monitReg13.has_key(place):
-			reg = monitReg13[place]
+		if self.monitReg13.has_key(place):
+			reg = self.monitReg13[place]
 
 			#pedir informacao
 		else:
 			return None
 
 	def comLamp1(self,place):
-		if comReg14.has_key(place):
-			reg = comReg14[place]
+		if self.comReg14.has_key(place):
+			reg = self.comReg14[place]
 
 			#pedir informacao
 		else:
 			return None
 
 	def comLamp2(self,place):
-		if comReg15.has_key(place):
-			reg = comReg15[place]
+		if self.comReg15.has_key(place):
+			reg = self.comReg15[place]
 
 			#pedir informacao
 		else:
 			return None
 
 	def comLamp3(self,place):
-		if comReg16.has_key(place):
-			reg = comReg16[place]
+		if self.comReg16.has_key(place):
+			reg = self.comReg16[place]
 
 			#pedir informacao
 		else:
