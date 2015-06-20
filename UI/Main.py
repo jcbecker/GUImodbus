2a22a3bb9a40a5a9405e16fa3e7870eb72b2fa44
marcoputon <marcoputon@gmail.com>
2015-06-19 21:42:55 -0300

    Evento de clique para mostrar temperaturas

--- UI/Main.py
+++ UI/Main.py
@@ -1,36 +1,35 @@
 from Tkinter import *
 import tkFont as font
-from ..Async.testingMonitor import testingMonitor
+from ..Async.TemperatureMonitor import TemperatureMonitor 
 
 class Main:
 
     def __init__( self, root ):
-        self.screenWidth = root.winfo_screenwidth()-10
-        self.screenHeight = root.winfo_screenheight()-10
+		self.screenWidth = root.winfo_screenwidth()-10
+		self.screenHeight = root.winfo_screenheight()-10
 
-        root.geometry(str(self.screenWidth)+"x"+str(self.screenHeight))
-        root.wm_title("Controle da casa")
-        root.configure(background="white")
+		root.geometry(str(self.screenWidth)+"x"+str(self.screenHeight))
+		root.wm_title("Controle da casa")
+		root.configure(background="white")
 
-        #distancia entre linhas.
-        self.distBetweenLines = self.screenHeight*0.01
+		#distancia entre linhas.
+		self.distBetweenLines = self.screenHeight*0.01
 
-        #distancia entre botoes na segunda linha.
-        self.distBetweenBtnSecLine = self.screenWidth*0.04
+		#distancia entre botoes na segunda linha.
+		self.distBetweenBtnSecLine = self.screenWidth*0.04
 
-        #distancia entre botoes na quarta linha.
-        self.distBetweenBtnFourthLine = self.screenWidth*0.1
-        self.labelHeight = 0.10*self.screenHeight
+		#distancia entre botoes na quarta linha.
+		self.distBetweenBtnFourthLine = self.screenWidth*0.1
+		self.labelHeight = 0.10*self.screenHeight
 
-        self.labelFont = font.Font(weight="normal",size=20)
-        self.btnFont = font.Font(weight="normal",size=14)
-        
-        self.tempMonitor = testingMonitor()
+		self.labelFont = font.Font(weight="normal",size=20)
+		self.btnFont = font.Font(weight="normal",size=14)
 
-        self.line1()
-        self.line2()
-        self.line3()
-        self.line4()
+		self.temMonitor = TemperatureMonitor()
+		self.line1()
+		self.line2()
+		self.line3()
+		self.line4()
 		
     def line1(self):
 
@@ -42,7 +41,7 @@ class Main:
 
     def line2(self):
 
-		self.wmBtn = Button( root, text = "Água", fg="blue", bg="white", activebackground="white",font=self.btnFont)
+		self.wmBtn = Button( root, text = "Agua", fg="blue", bg="white", activebackground="white",font=self.btnFont)
 		self.wmBtn.place(x=self.distBetweenBtnSecLine,
 				         y=self.labelHeight + 5,
 				         width=self.screenWidth*0.20,
@@ -54,7 +53,7 @@ class Main:
 		        		 width= self.screenWidth*0.20,
 		        		 height= self.screenHeight*0.10)
 
-		self.imBtn = Button( root, text = "Iluminação", fg="green", bg="white", activebackground="white",font=self.btnFont)
+		self.imBtn = Button( root, text = "Iluminacao", fg="green", bg="white", activebackground="white",font=self.btnFont)
 		self.imBtn.place(x = self.screenWidth*0.40 + self.distBetweenBtnSecLine*3,
 		        		 y = self.labelHeight + 5,
 		        		 width = self.screenWidth*0.20,
@@ -65,7 +64,8 @@ class Main:
 		        		 y = self.labelHeight + self.distBetweenLines,
 					     width = self.screenWidth*0.20,
 					     height = self.screenHeight*0.10)
-		self.tmBtn.bind("<Button-1>", self.tempMonitor.evento)
+		self.tmBtn.bind('<Button>', self.temMonitor.event )
+					     
 					
     def line3(self):
 
@@ -77,7 +77,7 @@ class Main:
 					   
     def line4(self):
 
-		self.waBtn = Button( root, text = "Água", fg="blue", bg="white", activebackground="white",font=self.btnFont)
+		self.waBtn = Button( root, text = "Agua", fg="blue", bg="white", activebackground="white",font=self.btnFont)
 		self.waBtn.place(x = self.distBetweenBtnFourthLine,
 				         y = 3*(self.labelHeight + self.distBetweenLines),
 				         width = self.screenWidth*0.20,
@@ -89,7 +89,7 @@ class Main:
 				         width = self.screenWidth*0.20,
 				         height = self.screenHeight*0.10)
 
-		self.iaBtn = Button( root, text = "Iluminação", fg="green", bg="white", activebackground="white",font=self.btnFont)
+		self.iaBtn = Button( root, text = "Iluminacao", fg="green", bg="white", activebackground="white",font=self.btnFont)
 		self.iaBtn.place(x = self.screenWidth*0.4 + self.distBetweenBtnFourthLine*3,
 				         y = 3*(self.labelHeight + self.distBetweenLines),
 				         width = self.screenWidth*0.20,
