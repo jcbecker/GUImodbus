1b45439c745f32efee275c99476899b077450ced
marcoputon <marcoputon@gmail.com>
2015-06-19 21:42:24 -0300

    Implementar busca pelo valor do alarme

--- Async/MyThread.py
+++ Async/MyThread.py
@@ -1,14 +1,8 @@
 from threading import Thread
 import time
 
+#implementar depois.
 class MyThread(Thread):
 
 	def __init__(self, m ):
-		Thread.__init__(self)
-		self.monitors = m
-
-	def run(self):
-		while True:
-			for i in self.monitors:
-				i.myTurn()
-			time.sleep(5)
+		pass
