10578b52e775bf5014e54e0524cb7d37325f9363
Matheus DallRosa <matheusdallrosa94@gmail.com>
2015-06-20 23:48:36 -0300

    Tabela para mostrar as temperaturas.

--- UI/TemperatureMonitor.py
+++ UI/TemperatureMonitor.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 from ..Resources.Temperature import Temperature
 import Tkinter as tk
+import Tkinter as tkk
 import tkFont as font
+import ttk
 import threading
 import time
 
@@ -16,44 +21,43 @@ class TemperatureMonitor(tk.Frame,threading.Thread):
 		self.stopQuery = False
 			
 	def run(self):
-		while not self.stopQuery:
-			children = self.parent.winfo_children()
-			for widget in children:
-				widget.destroy()
+		
+		xi=self.parent.winfo_x()+300
+		yi=self.parent.winfo_y()-300
+		xf=2*self.parent.winfo_x()+700
+		yf=self.parent.winfo_y()+40
+		while True:
+			if not self.stopQuery:
+				children = self.parent.winfo_children()
+				for widget in children:
+					widget.destroy()
 
-			l0 = tk.Label( self.parent, text = "Temperatura da Casa", font = font.Font(weight="normal",size=20), bg="white")
-			l0.pack(side="top",fill="both",expand= True,ipady=30)
+				temps = self.user.readTemp()
+				tree = ttk.Treeview(self.parent, columns=("Lugar","Temperatura"),
+									selectmode="extended",height=20)
+				tree["show"] = "headings"
+				tree.heading("#1", text="Lugar", anchor="center")
+				tree.heading("#2", text="Temperatura", anchor="center")
+				tree.column("#1", anchor="center", width=120)
+				tree.column("#2", anchor="center", width=120)
+				tree.insert("",0,text="", 
+							value=( "Piscina", str( int( temps[0], 16 ) ) + "°C" ) ) 
+				tree.insert("",1,text="", 
+							value=( "Banheira", str( int( temps[1], 16 ) ) + "°C") ) 
+				tree.insert("",2,text="", 
+							value=( "Suite", str( int( temps[2], 16 ) ) + "°C") ) 
+				tree.insert("",3,text="", 
+							value=( "Sala de estar", str( int( temps[3], 16 ) ) + "°C" ) ) 
+				tree.insert("",4,text="", 
+							value=( "Sala de jogos", str( int( temps[4], 16 ) ) + "°C" ) ) 
+				tree.insert("",5,text="", 
+							value=( "Dormitorio 1", str( int( temps[5], 16 ) ) + "°C" ) ) 
+				tree.insert("",6,text="", 
+							value=( "Dormitorio 2", str( int( temps[6], 16 ) ) + "°C" ) ) 
+				tree.place(	x=xi, y=yi,
+							width= xf, height= yf )
 
-			l1 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l1.pack(side="top",fill="both",expand = True,ipady=25)
-
-			l2 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l2.pack(side="top",fill="both",expand = True,ipady=25)
-
-			l3 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l3.pack(side="top",fill="both",expand = True,ipady=25)
-
-			l4 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l4.pack(side="top",fill="both",expand = True,ipady=25)
-
-			l5 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l5.pack(side="top",fill="both",expand = True,ipady=25)
-
-			l6 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l6.pack(side="top",fill="both",expand = True,ipady=25)		
-			
-			l7 = tk.Label( self.parent, bg="white", font = font.Font(weight="normal",size=14) )
-			l7.pack(side="top",fill="both",expand = True,ipady=25)
-			
-			temps = self.user.readTemp()
-
-			l1.configure( text = "Piscina: " + str( int( temps[0], 16 ) ) + " Graus", bg= "white" )
-			l2.configure( text = "Banheira: " + str( int( temps[1], 16 ) ) + " Graus", bg= "white" )			
-			l3.configure( text = "Suite: " + str( int( temps[2], 16 ) ) + " Graus", bg= "white" )
-			l4.configure( text = "Sala de estar: " + str( int( temps[3], 16 ) ) + " Graus", bg= "white" )
-			l5.configure( text = "Sala de jogos: " + str( int( temps[4], 16 ) ) + " Graus", bg= "white" )
-			l6.configure( text = "Dormitorio 1: " + str( int( temps[5], 16 ) ) + " Graus", bg= "white" )
-			l7.configure( text = "Dormitorio 2: " + str( int( temps[6], 16 ) ) + " Graus", bg= "white" )
-			self.ctrl.update()
-			print "monitor de temperatura dormindo.."
-			time.sleep(2)
+				self.parent.update()
+				print "monitor de temperatura dormindo.."
+			time.sleep(10)
+		print "monitor de temperatura morto."
